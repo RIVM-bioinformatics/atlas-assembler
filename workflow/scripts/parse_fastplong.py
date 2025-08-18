ec55393fa442b257880c570cf9168e795b030722
singhsp <sohana.singh@rivm.nl>
2025-08-18 12:05:11 +0200

    fix: bug in parsing fastplong data

--- workflow/scripts/parse_fastplong.py
+++ workflow/scripts/parse_fastplong.py
@@ -2,22 +2,13 @@ import sys
 import json
 import os
 
-if len(sys.argv) != 3:
-    print("Usage: python parse_fastplong.py <input.json> <output.tsv>")
+if len(sys.argv) < 3:
+    print("Usage: python parse_fastplong.py <input1.json> [<input2.json> ...] <output.tsv>")
     sys.exit(1)
 
-input_json = sys.argv[1]
-output_tsv = sys.argv[2]
-sample = os.path.splitext(os.path.basename(input_json))[0]
+input_jsons = sys.argv[1:-1]
+output_tsv = sys.argv[-1]
 
-with open(input_json) as f:
-    data = json.load(f)
-
-before = data['summary']['before_filtering']
-after = data['summary']['after_filtering']
-result = data['filtering_result']
-
-# Define columns for summary
 columns = [
     "sample",
     # Before filtering
@@ -28,11 +19,22 @@ columns = [
     "passed_filter_reads", "low_quality_reads", "too_many_N_reads", "too_short_reads", "too_long_reads"
 ]
 
-row = [sample]
-row += [before.get(k, "") for k in ['total_reads', 'total_bases', 'q20_bases', 'q30_bases', 'q20_rate', 'q30_rate', 'read_mean_length', 'gc_content']]
-row += [after.get(k, "") for k in ['total_reads', 'total_bases', 'q20_bases', 'q30_bases', 'q20_rate', 'q30_rate', 'read_mean_length', 'gc_content']]
-row += [result.get(k, "") for k in ['passed_filter_reads', 'low_quality_reads', 'too_many_N_reads', 'too_short_reads', 'too_long_reads']]
+rows = []
+
+for input_json in input_jsons:
+    sample = os.path.splitext(os.path.basename(input_json))[0]
+    with open(input_json) as f:
+        data = json.load(f)
+    before = data['summary']['before_filtering']
+    after = data['summary']['after_filtering']
+    result = data['filtering_result']
+    row = [sample]
+    row += [before.get(k, "") for k in ['total_reads', 'total_bases', 'q20_bases', 'q30_bases', 'q20_rate', 'q30_rate', 'read_mean_length', 'gc_content']]
+    row += [after.get(k, "") for k in ['total_reads', 'total_bases', 'q20_bases', 'q30_bases', 'q20_rate', 'q30_rate', 'read_mean_length', 'gc_content']]
+    row += [result.get(k, "") for k in ['passed_filter_reads', 'low_quality_reads', 'too_many_N_reads', 'too_short_reads', 'too_long_reads']]
+    rows.append(row)
 
 with open(output_tsv, "w") as out:
     out.write("\t".join(columns) + "\n")
-    out.write("\t".join(str(x) for x in row) + "\n")
+    for row in rows:
+        out.write("\t".join(str(x) for x in row) + "\n")
