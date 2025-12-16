01f8cf2cd813315896e5bb034d54c262af813448
singhsp <sohana.singh@rivm.nl>
2025-12-16 14:53:54 +0100

    fix: modify bash script to set working dir

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -9,6 +9,8 @@ set -euo pipefail
 input_dir="${1%/}"
 output_dir="${2%/}"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+cd ${DIR}
 #----------------------------------------------#
 ## make sure conda works
 
